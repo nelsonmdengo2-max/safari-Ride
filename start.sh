9ce8540a677582c7762184191245043be66780ec
nelsonmdengo2-max <nelsonmdengo2@gmail.com>
2026-07-05 16:16:01 -0700

    Fix port for Railway

--- start.sh
+++ start.sh
@@ -4,4 +4,6 @@ if [ ! -f .env ]; then
 fi
 php artisan key:generate --force || true
 php artisan migrate --force || true
+sed -i "s/80/${PORT}/g" /etc/apache2/ports.conf
+sed -i "s/80/${PORT}/g" /etc/apache2/sites-available/000-default.conf
 apachectl start

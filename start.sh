b90f5894af3e46897906623d47098f141be85e47
nelsonmdengo2-max <nelsonmdengo2@gmail.com>
2026-07-05 16:52:58 -0700

    Fix start script

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ php artisan key:generate --force || true
 php artisan migrate --force || true
 sed -i "s/Listen 80/Listen ${PORT}/g" /etc/apache2/ports.conf
 sed -i "s/*:80/*:${PORT}/g" /etc/apache2/sites-available/000-default.conf
-exec apachectl -D FOREGROUND
+exec apache2-foreground

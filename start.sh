622214619318b0341141c68d7f63aa69818d730d
nelsonmdengo2-max <nelsonmdengo2@gmail.com>
2026-07-05 17:01:06 -0700

    Fix MPM and firebase file

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 if [ ! -f .env ]; then
   cp .env.save .env
 fi
+mkdir -p /var/www/html/public/push-configurations
+echo '{}' > /var/www/html/public/push-configurations/bidding_firebase.json
 php artisan key:generate --force || true
 php artisan migrate --force || true
 sed -i "s/Listen 80/Listen ${PORT}/g" /etc/apache2/ports.conf

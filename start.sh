0f866bed10524e435494c474da3641b75e5768ec
nelsonmdengo2-max <nelsonmdengo2@gmail.com>
2026-07-05 18:14:03 -0700

    Fix php-fpm command

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ if [ ! -f .env ]; then cp .env.save .env; fi
 php artisan key:generate --force || true
 php artisan migrate --force || true
 echo "server { listen ${PORT}; root /var/www/html/public; index index.php; location / { try_files \$uri \$uri/ /index.php?\$query_string; } location ~ \.php$ { fastcgi_pass unix:/run/php/php8.2-fpm.sock; fastcgi_index index.php; include fastcgi_params; fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name; } }" > /etc/nginx/sites-available/default
-php-fpm8.2
+php-fpm
 exec nginx -g 'daemon off;'

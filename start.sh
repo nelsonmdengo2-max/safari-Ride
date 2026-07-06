d97e8468bf6e895e1be2d2f61cd3c91e1187e5b8
nelsonmdengo2-max <nelsonmdengo2@gmail.com>
2026-07-05 18:33:43 -0700

    Fix file paths

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+cd /var/www/html
 if [ ! -f .env ]; then cp .env.save .env; fi
 php artisan key:generate --force || true
 php artisan migrate --force || true
-echo "server { listen ${PORT}; root /var/www/html/public; index index.php; location / { try_files \$uri \$uri/ /index.php?\$query_string; } location ~ \.php$ { fastcgi_pass 127.0.0.1:9000; fastcgi_index index.php; include fastcgi_params; fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name; } }" > /etc/nginx/sites-available/default
 sed -i 's|listen = .*|listen = 127.0.0.1:9000|' /usr/local/etc/php-fpm.d/www.conf
+echo "server { listen ${PORT}; root /var/www/html/public; index index.php; location / { try_files \$uri \$uri/ /index.php?\$query_string; } location ~ \.php$ { fastcgi_pass 127.0.0.1:9000; fastcgi_index index.php; include fastcgi_params; fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name; } }" > /etc/nginx/sites-available/default
 exec /usr/bin/supervisord -c /var/www/html/supervisord.conf

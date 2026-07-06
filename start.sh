f53ee92e67120b8a988c6dde8b080d174933986e
nelsonmdengo2-max <nelsonmdengo2@gmail.com>
2026-07-05 18:21:13 -0700

    Fix php-fpm listener

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 if [ ! -f .env ]; then cp .env.save .env; fi
 php artisan key:generate --force || true
 php artisan migrate --force || true
+sed -i 's|listen = .*|listen = 127.0.0.1:9000|' /usr/local/etc/php-fpm.d/www.conf
 echo "server { listen ${PORT}; root /var/www/html/public; index index.php; location / { try_files \$uri \$uri/ /index.php?\$query_string; } location ~ \.php$ { fastcgi_pass 127.0.0.1:9000; fastcgi_index index.php; include fastcgi_params; fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name; } }" > /etc/nginx/sites-available/default
-php-fpm -d
-sleep 2
+php-fpm
 exec nginx -g 'daemon off;'
